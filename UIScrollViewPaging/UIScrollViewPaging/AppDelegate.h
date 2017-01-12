0d8682a5f48505d0b907ecc343643284de0d6795
snow <snow@snowdeMBP.lan>
2017-01-12 21:56:51 +0800

    init the repository

---
+++ UIScrollViewPaging/UIScrollViewPaging/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIScrollViewPaging
+//
+//  Created by snow on 17/1/12.
+//  Copyright © 2017年 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIScrollViewPaging/UIScrollViewPaging/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIScrollViewPaging
+//
+//  Created by snow on 17/1/12.
+//  Copyright © 2017年 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
